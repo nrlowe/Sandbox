37a2ef7c3054d44678ee21b035d18bf5604c94a3
nrlowe <nate.lowe.2013@owu.edu>
2025-09-14 15:13:55 -0400

    updates

--- src/main/java/com/lowe/Sandbox/business/InterviewQs.java
+++ src/main/java/com/lowe/Sandbox/business/InterviewQs.java
@@ -2,6 +2,7 @@ package com.lowe.Sandbox.business;
 
 import java.text.Collator;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class InterviewQs {
@@ -36,8 +37,8 @@ public class InterviewQs {
         for(int i = 0; i < n; i++){
             sb.append(s.charAt(i));
             int j = sb.length() - 1;
-            cnts[j] = 1 + (j > 0 && sb.charAt(j - 1) == s.charAt(i) ? cnts[j - 1] : 0);
-            if (cnts[j] == d) {
+            cnts[j] = 1 + (j > 0 && s.charAt(i) == sb.charAt(j - 1) ? cnts[j - 1] : 0);
+            if(cnts[j] == d){
                 sb.setLength(sb.length() - d);
             }
         }
@@ -135,5 +136,68 @@ public class InterviewQs {
             left[ch]++;
         }
         return ans % MOD;
+    }
+
+//Similar to above but now want to find A B A B patterns in a string
+
+
+    public static long ababPattern(String s) {
+        long ans = 0;
+        long[] left = new long[ALPH];
+        long[] right = new long[ALPH];
+
+        for(int i = 0; i < s.length(); i++){
+            right[s.charAt(i)]++;
+        }
+
+        long[][] l = new long[ALPH][ALPH];
+
+        for (int k = 0; k < s.length(); k++) {
+            char ch = s.charAt(k);
+            right[ch]--;
+            long[] sb = l[ch];
+            for(int a = 0; a < ALPH; a++){
+                if(sb[a] != 0 && right[a] != 0){
+                   ans+= sb[a] * right[a];
+                }
+            }
+
+            for(int a = 0; a < ALPH; a++){
+                if(left[a] != 0){
+                    sb[a] += left[a];
+                }
+            }
+            left[ch]++;
+        }
+        return ans % MOD;
+    }
+
+
+//DNA from DNA fragments (no overlap)
+    public static boolean dnaFragments(String dna, List<String> fragments){
+        for(String s : fragments){
+            int sx = 0;
+            int start = 0;
+            for(int i = 0; i < dna.length(); i++){
+                if(sx == s.length() - 1 && s.charAt(i) == dna.charAt(i)){
+                    StringBuilder sb = new StringBuilder(dna);
+                    sb.delete(start, i);
+                    dna = sb.toString();
+                }
+                if(s.charAt(i) == dna.charAt(i) && i + (s.length() - sx) < dna.length()){
+                    sx++;
+                } else {
+                    sx = 0;
+                    start = i++;
+                }
+            }
+        }
+        return dna.length() > 0 ? false : true;
+    }
+
+//DNA from DNA fragments (no overlap) with min amount of fragments
+    public static boolean dnaFragmentsMin(String dna, List<String> fragments){
+
+        return false;
     }
 }
